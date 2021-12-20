3e2d8cc70d212663e0ee4bbae8bc86ae0115229a
WaleedK2000 <74948618+WaleedK2000@users.noreply.github.com>
2021-12-20 16:48:01 +0500

    added credentials test

---
+++ app/src/androidTest/java/com/example/pms/Controller/CredentialsTest.java
@@ -0,0 +1,53 @@
+package com.example.pms.Controller;
+
+import static org.junit.Assert.*;
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+public class CredentialsTest {
+
+    Credentials credentials;
+    String username;
+    String password;
+    Context context;
+
+
+    @Before
+    public void setUp()  {
+
+        credentials = Credentials.getInstance();
+        username = "test123";
+        password = "ab434433gtgtc";
+        context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+    }
+
+    @Test
+    public void signUp() {
+
+        assertTrue(credentials.signUp(username,password,context));
+
+    }
+    @Test
+    public void invalidSignup(){
+        assertFalse(credentials.signUp(username,"1", context));
+    }
+    @Test
+    public void logIn(){
+        assertTrue(credentials.logIn(username,password,context));
+
+    }
+    @Test
+    public void invalidLogin(){
+        assertFalse(credentials.logIn("XyC", "Xyyyyvvv", context));
+    }
+
+
+}
